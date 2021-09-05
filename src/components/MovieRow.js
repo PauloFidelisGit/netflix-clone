f0d3d600b37474857ad603ef8acbe373b8ddffe9
Paulo Fidélis <paulofidelisdev@gmail.com>
2021-09-05 03:37:41 -0400

    em construçao...

---
+++ src/components/MovieRow.js
@@ -0,0 +1,12 @@
+import './MovieRow.css';
+
+export default ({ title, items }) => (
+  <div>
+    <h2>{title}</h2>
+    <div className="movieRow--listarea">
+      {items.results.length > 0 && items.results.map((item, key) => (
+        <img src={`https://image.tmdb.org/t/p/w300${item.poster_path}`} alt="" />
+      ))}
+    </div>
+  </div>
+);
